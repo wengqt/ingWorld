4e120259dd26b91b4f6701c86494b3d5b4a5f930
w1014804721 <17854160637@163.com>
2017-04-27 14:03:04 +0800

    a

--- src/main/java/com/ingzone/base/ResultStatusMessage.java
+++ src/main/java/com/ingzone/base/ResultStatusMessage.java
@@ -5,6 +5,7 @@ package com.ingzone.base;
  */
 public interface ResultStatusMessage {
     String M_OK = "ok";
-    String M_FAILURE = "failure ";
+    String M_FAILURE = "failure";
     String M_PERMISSION_DENIED = "permission denied";
 }
+

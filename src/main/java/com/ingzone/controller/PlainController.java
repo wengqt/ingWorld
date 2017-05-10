52aa0de86c3362cb24676e68b629a763c3f3a795
Your Name <you@example.com>
2017-05-10 23:07:31 +0800

    工作室介绍

--- src/main/java/com/ingzone/controller/PlainController.java
+++ src/main/java/com/ingzone/controller/PlainController.java
@@ -1,6 +1,12 @@
 package com.ingzone.controller;
 
+import com.ingzone.base.Result;
+import com.ingzone.cache.ResultCache;
+import com.ingzone.model.dto.IngDTO;
+import com.ingzone.service.IngService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,5 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/plain")
 public class PlainController{
+
+    @Autowired
+    IngService ingService;
+
+    @RequestMapping(value = "/getStudioIntro",method = RequestMethod.GET)
+    public Result getStudioIntro() {
+        return ResultCache.getDataOk(ingService.getStudioIntro());
+    }
+
+    @RequestMapping(value = "/modifyStudio",method = RequestMethod.POST)
+    public Result modifyStudio(IngDTO ingDTO) {
+        if (ingService.modifyStudio(ingDTO)){
+            return ResultCache.OK;
+        }
+        else{
+            return ResultCache.FAILURE;
+        }
+    }
 
 }

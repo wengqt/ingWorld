e36c76e96cef12e9027e8eef6ea3ac99122c533b
quyans@sina.com <quyans@sina.com>
2017-05-13 17:20:56 +0800

    已经写完了

---
+++ src/main/webapp/js/FrontIndexWithDiamond.js
@@ -0,0 +1,20 @@
+/**
+ * Created by 延松松松松 on 2017/5/13.
+ */
+
+document.getElementById("head_1").onclick=function () {
+    document.getElementsByClassName("mainBody").style.display="none";
+    document.getElementById("mainBody_1").style.display="block";
+}
+document.getElementById("head_2").onclick=function () {
+    document.getElementsByClassName("mainBody").style.display="none";
+    document.getElementById("mainBody_2").style.display="block";
+}
+document.getElementById("head_3").onclick=function () {
+    document.getElementsByClassName("mainBody").style.display="none";
+    document.getElementById("mainBody_3").style.display="block";
+}
+document.getElementById("head_4").onclick=function () {
+    document.getElementsByClassName("mainBody").style.display="none";
+    document.getElementById("mainBody_4").style.display="block";
+}

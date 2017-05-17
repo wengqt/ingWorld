5fe16a77bb14caad6bc0c22f7744386e5be7ebcb
zhou haoxiang <835390899@qq.com>
2017-05-17 21:53:52 +0800

    样式还差翻页。

---
+++ src/main/webapp/js/RecruitmentContent.js
@@ -0,0 +1,18 @@
+var addimg=document.getElementById("addimg");
+addimg.onchange=function() {
+	var url = getFileURL(); 
+  	var img= document.getElementById("imgPre"); 
+  	img.src= url;
+}
+function getFileURL(){
+	var url; 
+	if (navigator.userAgent.indexOf("MSIE")>=1) { // IE 
+		url = document.getElementById('addimg').value; 
+	} else if(navigator.userAgent.indexOf("Firefox")>0) { // Firefox 
+		url = window.URL.createObjectURL(document.getElementById('addimg').files.item(0)); 
+	} else if(navigator.userAgent.indexOf("Chrome")>0) { // Chrome 
+		url = window.URL.createObjectURL(document.getElementById('addimg').files.item(0)); 
+	} 
+	console.log(url);
+return url; 
+}

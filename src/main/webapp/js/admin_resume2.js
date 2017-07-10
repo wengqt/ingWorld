b8ad43391587016f7ec28b1b6b2ec759ef4afbd8
zhou haoxiang <835390899@qq.com>
2017-07-10 16:51:55 +0800

    翻页效果实现

--- src/main/webapp/js/admin_resume2.js
+++ src/main/webapp/js/admin_resume2.js
@@ -1,11 +1,28 @@
 /**
  * Created by zhou on 2017/7/8.
  */
+var changeResume=document.getElementById("changeResume");
+var submitChange=document.getElementById("submitChange");
+var input=document.getElementsByClassName("input");
+var textarea=document.getElementsByClassName("inputtext")
 $.ajax({
     url:API.getUserInfo,
     type:"GET",
     success:function (data) {
-        var json=JSON.parse(data);
-        console.log(json);
+
 }
 })
+
+changeResume.onclick=function () {
+    changeResume.style.display="none";
+    submitChange.style.display="block";
+
+}
+submitChange.onclick=function () {
+    changeResume.style.display="block";
+    submitChange.style.display='none';
+    $.ajax({
+        url:API.modifyUserInfo,
+
+    })
+}

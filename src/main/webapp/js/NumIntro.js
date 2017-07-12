3d5c36ee23bac5c90bbe14a046724ace82091380
quyans@sina.com <quyans@sina.com>
2017-07-12 21:55:45 +0800

    写了if语句来添加监听器但是效果不理想  需要求助大佬

--- src/main/webapp/js/NumIntro.js
+++ src/main/webapp/js/NumIntro.js
@@ -3,39 +3,3 @@
  */
 
 
-$.ajax(
-    {
-        url:API.GroupNumIntro,
-        type:"GET",
-        dataType:"json",
-        success:function (data) {
-            console.log("suc");
-            for(var i = 0;i<data.members.length;i++)
-            {
-                var personalIntroDiv = document.createElement("div");
-                personalIntroDiv.className = "personalIntroDiv";
-
-                var PerIntrDiv = document.createElement("div");
-                PerIntrDiv.className = "diamondDiv_group_1";
-
-                var personalIntroText = document.createElement("div");
-                personalIntroText.className = "personalIntroText";
-                console.log("1");
-
-
-                var personalIntroTextP = document.createElement("p");
-                personalIntroTextP.className = "personalIntroTextP";
-                personalIntroTextP.innerHTML = data.members[i].name+data.members[i].introduce+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-
-                personalIntroText.appendChild(personalIntroTextP);
-                personalIntroDiv.appendChild(PerIntrDiv);
-                personalIntroDiv.appendChild(personalIntroText);
-                document.getElementById("groupMumIntr").appendChild(personalIntroDiv)
-            }
-        },
-        error:function (a,b,c) {
-            console.log(a,b,c)
-        }
-
-    }
-);

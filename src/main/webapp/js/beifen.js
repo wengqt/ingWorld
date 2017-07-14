928b5b94170f449c600f8081c8a45054efe13534
quyans@sina.com <quyans@sina.com>
2017-07-14 15:04:40 +0800

    组别成员介绍界面完成

--- src/main/webapp/js/beifen.js
+++ src/main/webapp/js/beifen.js
@@ -1,3 +1,9 @@
+/**
+ * Created by 延松松松松 on 2017/7/14.
+ */
+/**
+ * Created by 延松松松松 on 2017/7/10.
+ */
 function  getGroupNumber(num) {
 
     if(a.style.display == "none"){
@@ -48,51 +54,51 @@ function  getGroupNumber(num) {
 
 
 
-function getGroupIntro(num) {
-
-    var GroupName;
-
-    switch (num){
-
-        case 1:  GroupName = "后台组介绍";
-            break;
-        case 2:  GroupName = "前端组介绍";
-            break;
-        case 3:  GroupName = "安卓组介绍";
-            break;
-        case 4:  GroupName = "IOS组介绍";
-            break;
-        case 5:  GroupName = "游戏组介绍";
-            break;
-        case 6:  GroupName = "设计组介绍";
-            break;
-    }
-
-          console.log(num);
-
-    $.ajax(
-        {
-            url:API.GroupNumIntro+"?groupId="+num,
-            type:"GET",
-            dataType:"json",
-
-            success:function (data) {
-
-                document.getElementsByClassName("groupName").innerHTML = GroupName;
-                document.getElementsByClassName("IntroContent").innerHTML = data.introduce;
-
-                console.log(data.introduce);
-
-
-            },
-            error:function (a,b,c) {
-                console.log(a,b,c)
-            }
-
-        }
-    );
-
-}
+// function getGroupIntro(num) {
+//
+//     var GroupName;
+//
+//     switch (num){
+//
+//         case 1:  GroupName = "后台组介绍";
+//             break;
+//         case 2:  GroupName = "前端组介绍";
+//             break;
+//         case 3:  GroupName = "安卓组介绍";
+//             break;
+//         case 4:  GroupName = "IOS组介绍";
+//             break;
+//         case 5:  GroupName = "游戏组介绍";
+//             break;
+//         case 6:  GroupName = "设计组介绍";
+//             break;
+//     }
+//
+//           console.log(num);
+//
+//     $.ajax(
+//         {
+//             url:API.GroupNumIntro+"?groupId="+num,
+//             type:"GET",
+//             dataType:"json",
+//
+//             success:function (data) {
+//
+//                 document.getElementsByClassName("groupName").innerHTML = GroupName;
+//                 document.getElementsByClassName("IntroContent").innerHTML = data.introduce;
+//
+//                 console.log("我成功得到了组别信息");
+//
+//
+//             },
+//             error:function (a,b,c) {
+//                 console.log(a,b,c)
+//             }
+//
+//         }
+//     );
+//
+// }
 
 
 
@@ -123,7 +129,7 @@ for(var i = 0;i<showButton.length;i++){
 
 
             getGroupNumber(index+1);
-            getGroupIntro(index+1);
+            // getGroupIntro(index+1);
 
             ShowGroupNumber();
         }
@@ -131,3 +137,5 @@ for(var i = 0;i<showButton.length;i++){
 
     })(i)) ;
 }
+
+
